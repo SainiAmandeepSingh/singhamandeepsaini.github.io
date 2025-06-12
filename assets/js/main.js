f1869a090086166d4295ab51c50180b126e36458
Singh AmanDeep Saini <121757220+SainiAmandeepSingh@users.noreply.github.com>
2025-06-12 20:28:43 +0200

    Add scroll-up button

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,3 +83,11 @@ function navHighlighter()
     })
 }
 /*=============== SHOW SCROLL UP ===============*/
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollUp);
